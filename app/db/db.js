2a63ef44070ed115386145af9a1935a6e2420bc7
aliezaheer <alizaheer67@gmail.com>
2023-07-14 14:19:12 +0500

    form issue on netlify

--- app/db/db.js
+++ app/db/db.js
@@ -2,6 +2,7 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 
+//connection
 export async function connectToDatabase() {
   const client = await MongoClient.connect(uri);
   return client;
