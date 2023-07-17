4079b4ef4970a47d32ba4de2af2ee487f204841b
aliezaheer <alizaheer67@gmail.com>
2023-07-18 02:17:41 +0500

    initial screen loader components

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,22 @@
+"use client";
+import Loading from "@/components/loading/loading";
+import "../styles/globals.css";
+
+import React, { useState, useEffect } from "react";
+
 import Hero from "@/components/hero/Hero";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous task
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer); // Clean up the timer on unmount
+  }, []);
+
+  return isLoading ? <Loading /> : <Hero />;
 }
