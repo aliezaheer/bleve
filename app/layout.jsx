f6f6dbd39de104837a91d58c6e7a809de30c88f8
aliezaheer <alizaheer67@gmail.com>
2023-07-18 20:06:45 +0500

    swiper slider pkg and css fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+
 import "../styles/globals.css";
 
 import { Inter } from "next/font/google";
--- components/HomePage/BeforeFooter.jsx
+++ components/HomePage/BeforeFooter.jsx
@@ -2,28 +2,32 @@ import Link from "next/link";
 
 const BeforeFooter = () => {
   return (
-    <div className="flex flex-wrap md:flex-nowrap justify-around items-center bg-white text-black h-60">
-      <div>
-        <h2 className="text-3xl ">
-          Get done <br />
-          Your next
-          <br />
-          <span className="text-hoverRed text-5xl font-bold pl-3">Project</span>
-          <br />
-          <span className=""> With Us</span>
-        </h2>
+    <>
+      <div className="flex flex-wrap md:flex-nowrap justify-around items-center bg-white text-black h-60">
+        <div>
+          <h2 className="text-3xl ">
+            Get done <br />
+            Your next
+            <br />
+            <span className="text-hoverRed text-5xl font-bold pl-3">
+              Project
+            </span>
+            <br />
+            <span className=""> With Us</span>
+          </h2>
+        </div>
+        <div>
+          <Link href="/contact">
+            <button
+              className="bg-red hover:bg-black text-white hover:opacity-100 text-lg py-5 px-8 mt-4 ml-0 md:ml-14 rounded-full"
+              variant="contained"
+            >
+              Get In Touch
+            </button>
+          </Link>
+        </div>
       </div>
-      <div>
-        <Link href="/contact">
-          <button
-            className="bg-red hover:bg-black text-white hover:opacity-100 text-lg py-5 px-8 mt-4 ml-0 md:ml-14 rounded-full"
-            variant="contained"
-          >
-            Get In Touch
-          </button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
