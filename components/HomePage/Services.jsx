a11a8385a88cde632d4656df8e8183c0951bfc05
aliezaheer <alizaheer67@gmail.com>
2023-07-19 13:35:27 +0500

    links fixed

--- components/HomePage/Services.jsx
+++ components/HomePage/Services.jsx
@@ -6,45 +6,47 @@ import BusinessProcess from "./BusinessProcess";
 
 const Services = () => {
   return (
-    <div className="after-hero bg-lightPurple -z-40 mt-96 md:mt-0 ">
-      <div className="service-sect bg-secondSec flex flex-col md:flex-row justify-around h-full transform rotate-0 md:transform md:-rotate-1 -z-10 pl-4 md:pl-1 py- ">
-        <div className="mt-20">
-          <h2 className="text-2xl md:text-8xl md:ml-20 w-full leading-none">
-            <span className="relative">
-              <span className="absolute top-0 left-0 transform -skew-y-6 bg-gray-100 h-8 w-24 -ml-4"></span>
-              Services
-            </span>
-            <br />
-            <span className="text-matchingTextCyan text-6xl font-bold pl-0 md:pl-4">
-              Brand, Build, Grow
-            </span>
-            <br />
-            <span className=" ml-5 md:-ml-4">INSTANTLY</span>
-          </h2>
-          <div className="flex flex-wrap flex-col-reverse align-middle">
-            <div>
-              <button
-                className="bg-white text-secondSec hover:opacity-70 text-lg py-5 px-8 mt-4 md:ml-14 rounded-full"
-                variant="contained"
-              >
-                LernMore
-              </button>
-            </div>
-            <div className="w-78 md:w-60 text-xs ml-0 md:ml-16 py-4 ">
-              Our team analyzes and measures your brand’s message and online
-              health with an in-depth brand analysis. We highlight your brand’s
-              strengths, weaknesses, threats, and opportunities. Then we develop
-              a brand story and strategy so you can truly connect with your
-              future loyal customers.
+    <section id="services">
+      <div className="after-hero bg-lightPurple -z-40 mt-96 md:mt-0 ">
+        <div className="service-sect bg-secondSec flex flex-col md:flex-row justify-around h-full transform rotate-0 md:transform md:-rotate-1 -z-10 pl-4 md:pl-1 py- ">
+          <div className="mt-20">
+            <h2 className="text-2xl md:text-8xl md:ml-20 w-full leading-none">
+              <span className="relative">
+                <span className="absolute top-0 left-0 transform -skew-y-6 bg-gray-100 h-8 w-24 -ml-4"></span>
+                Services
+              </span>
+              <br />
+              <span className="text-matchingTextCyan text-6xl font-bold pl-0 md:pl-4">
+                Brand, Build, Grow
+              </span>
+              <br />
+              <span className=" ml-5 md:-ml-4">INSTANTLY</span>
+            </h2>
+            <div className="flex flex-wrap flex-col-reverse align-middle">
+              <div>
+                <button
+                  className="bg-white text-secondSec hover:opacity-70 text-lg py-5 px-8 mt-4 md:ml-14 rounded-full"
+                  variant="contained"
+                >
+                  LernMore
+                </button>
+              </div>
+              <div className="w-78 md:w-60 text-xs ml-0 md:ml-16 py-4 ">
+                Our team analyzes and measures your brand’s message and online
+                health with an in-depth brand analysis. We highlight your
+                brand’s strengths, weaknesses, threats, and opportunities. Then
+                we develop a brand story and strategy so you can truly connect
+                with your future loyal customers.
+              </div>
             </div>
           </div>
+          <div className=" ">
+            <Image src={servicesBg} alt="" />
+          </div>
         </div>
-        <div className=" ">
-          <Image src={servicesBg} alt="" />
-        </div>
+        <BusinessProcess />
       </div>
-      <BusinessProcess />
-    </div>
+    </section>
   );
 };
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,39 @@
 "use client";
-
-import Image from "next/image";
+import { useState, useEffect } from "react";
 import logo from "../public/logo.png";
-import hamburger from "../public/hamburger.png";
-import close from "../public/close.png";
-import { slide as Menu } from "react-burger-menu";
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-  const showSettings = (event) => {
-    event.preventDefault();
-    // Your logic for handling settings goes here
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  // Function to close the menu
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  // Attach event listeners to links and menu items when the component mounts
+  useEffect(() => {
+    const links = document.querySelectorAll(".navigation__link");
+    links.forEach((link) => {
+      link.addEventListener("click", closeMenu);
+    });
+
+    // Cleanup: Remove event listeners when the component unmounts
+    return () => {
+      links.forEach((link) => {
+        link.removeEventListener("click", closeMenu);
+      });
+    };
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
+  // Toggle menu open/closed state
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   return (
-    <div className="flex justify-between px-8 ">
+    <div className="flex justify-between px-8">
       <div>
         <Link href="/">
           <Image
@@ -32,42 +51,53 @@ const Navbar = () => {
           type="checkbox"
           className="navigation__checkbox"
           id="nav-toggle"
+          checked={isMenuOpen}
+          onChange={handleMenuToggle}
         />
         <label htmlFor="nav-toggle" className="navigation__button">
           <span
             className="navigation__icon"
             aria-label="toggle navigation menu"
-          ></span>
+          >
+            {isMenuOpen ? (
+              // Replace this with your custom close icon or font icon
+              <i className="custom-close-icon"></i>
+            ) : (
+              // Replace this with your custom hamburger icon or font icon
+              <i className="custom-hamburger-icon"></i>
+            )}
+          </span>
         </label>
         <div className="navigation__background"></div>
 
         <nav className="navigation__nav" role="navigation">
           <ul className="navigation__list">
             <li className="navigation__item">
-              <a href="#" className="navigation__link">
+              <Link href="/" className="navigation__link">
                 <span>01</span>Home
-              </a>
+              </Link>
             </li>
             <li className="navigation__item">
-              <a href="#about" className="navigation__link">
+              <Link href="/#services" className="navigation__link">
                 <span>02</span>About
-              </a>
+              </Link>
+              <li className="navigation__item">
+                <Link href="#" className="navigation__link">
+                  <span>03</span>Reviews
+                </Link>
+              </li>
+              <li className="navigation__item">
+                <Link href="#" className="navigation__link">
+                  <span>04</span>Services
+                </Link>
+              </li>
+              <li className="navigation__item">
+                <Link href="#" className="navigation__link">
+                  <span>05</span>Contact
+                </Link>
+              </li>
             </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>03</span>Reviews
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>04</span>Services
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>05</span>Contact
-              </a>
-            </li>
+            {/* ... (other menu items without any onClick event) */}
           </ul>
         </nav>
       </div>
